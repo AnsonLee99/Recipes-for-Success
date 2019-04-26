66f0dd993ed59baf4fb15c4e99662527da9e792b
Ruben <ruc064@ucsd.edu>
2019-04-26 15:41:36 -0700

    Added menu to recipes page, and handler for search function

--- app/src/main/java/com/example/recipesforsuccess/Recipes.java
+++ app/src/main/java/com/example/recipesforsuccess/Recipes.java
@@ -1,12 +1,18 @@
 package com.example.recipesforsuccess;
 
-import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
+import android.view.MenuInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.app.SearchManager;
+import android.widget.SearchView;
+import android.content.Context;
+import android.widget.TextView;
+import android.provider.SearchRecentSuggestions;
+
 
 public class Recipes extends MainPage {
     LinearLayout mainDisplay;
@@ -21,11 +27,59 @@ public class Recipes extends MainPage {
 
         // For displaying the currently selected tab
         // I can't fuckin figure it out
-        RadioGroup rg = (RadioGroup) findViewById(R.id.NavBar_Group);
-        RadioButton curr = (RadioButton)findViewById(R.id.recipes_tab_button);
+        //RadioGroup rg = (RadioGroup) findViewById(R.id.NavBar_Group);
+        //RadioButton curr = (RadioButton)findViewById(R.id.recipes_tab_button);
         //curr.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.<CLICKED VERSION OF ICON>);
         //curr.setTextColor(Color.parseColor("3F51B5"));
+
+        handleIntent(getIntent());
+
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+
+        // Get SearchView and set configuration from /res/xml/searchable.xml
+        SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
+        return true;
+    }
+
+ /*   @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.search_bar:
+                TextView txt = (TextView) findViewById(R.id.textView2);
+                txt.setText("hi searb-bar");
+                return true;
+        }
+        return true;
+    }*/
+
+    @Override
+    protected void onNewIntent(Intent intent){
+        setIntent(intent);
+        handleIntent(intent);
+    }
+
+    /**
+     * Handles search queries when user clicks on search button after entering text
+     * @param intent
+     */
+    private void handleIntent(Intent intent){
+        if( Intent.ACTION_SEARCH.equals(intent.getAction()) ){
+            //setContentView(R.layout.activity_search_results);
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            TextView txt = (TextView) findViewById(R.id.recipe_text);
+            txt.setText("Your query is: " + query);
+        }
+
+    }
     // TODO
 }
