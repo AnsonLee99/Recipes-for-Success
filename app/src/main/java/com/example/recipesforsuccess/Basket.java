cb73be09783b40c8c107a6f37c539cb370d26235
anson <all197@ucsd.edu>
2019-05-04 19:00:07 -0700

    Autofill for Basket Search

--- app/src/main/java/com/example/recipesforsuccess/Basket.java
+++ app/src/main/java/com/example/recipesforsuccess/Basket.java
@@ -1,8 +1,13 @@
 package com.example.recipesforsuccess;
+import android.os.Debug;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.RadioButton;
@@ -10,11 +15,29 @@ import android.widget.RadioGroup;
 
 import com.example.recipesforsuccess.dataobjects.FoodListViewAdapter;
 import com.example.recipesforsuccess.dataobjects.FoodListViewItem;
+import com.fasterxml.jackson.databind.JsonNode;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class Basket extends MainPage {
     LinearLayout mainDisplay;
+    private AutoCompleteTextView bar;
+    private ArrayAdapter<String> options;
+    private JSONArray res;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +53,49 @@ public class Basket extends MainPage {
         //curr.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.<CLICKED VERSION OF ICON>);
         //curr.setTextColor(Color.parseColor("3F51B5"));
 
+
+        // Auto-complete searchbar
+        bar = (AutoCompleteTextView) findViewById(R.id.basket_searchBar);
+        options = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, new ArrayList<String>());
+        bar.setAdapter(options);
+        //bar.setDropDownHeight(6);
+        options.notifyDataSetChanged();
+
+        bar.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                Log.d("test", "NEW ENTRY: " + s.toString());
+
+                ExecutorService service = Executors.newSingleThreadExecutor();
+                CustomCallable call = new CustomCallable(s.toString());
+                Future<JSONArray> f = service.submit(call);
+
+                options.clear();
+                ArrayList<String> currOptions = new ArrayList<>();
+                try {
+                    res = f.get();
+                    for ( int i = 0; i < res.length(); i ++ ) {
+                        currOptions.add(res.getJSONObject(i).get("name").toString());
+                    }
+                } catch(Exception e) {
+                    Log.d("test", "EXCEPTION WITH RETRIEVING ARRAYLIST: " + e);
+                }
+                service.shutdown();
+
+                options.addAll(currOptions);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                options.notifyDataSetChanged();
+            }
+        });
+
         ListView listView = (ListView) findViewById(R.id.basket_list_view);
 
         ArrayList<FoodListViewItem> content = new ArrayList<FoodListViewItem>();
@@ -44,6 +110,9 @@ public class Basket extends MainPage {
         listView.setAdapter(adapter);
 
     }
+
+
+
 
     // TODO
 }
