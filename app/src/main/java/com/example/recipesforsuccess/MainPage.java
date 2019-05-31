f279e023ec47bbc88538f2cd4ee1dccbead76d67
janAntu <randrus@ucsd.edu>
2019-05-30 17:39:04 -0700

    Deleted social files

--- app/src/main/java/com/example/recipesforsuccess/MainPage.java
+++ app/src/main/java/com/example/recipesforsuccess/MainPage.java
@@ -1,22 +1,8 @@
 package com.example.recipesforsuccess;
 
-import com.example.recipesforsuccess.Basket;
-import com.example.recipesforsuccess.GroceryList;
-import com.example.recipesforsuccess.MainActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.RadioGroup;
-
-import com.example.recipesforsuccess.Profile;
-import com.example.recipesforsuccess.R;
-import com.example.recipesforsuccess.Recipes;
-import com.example.recipesforsuccess.Social;
-import com.google.firebase.auth.FirebaseAuth;
 
 public class MainPage extends MainActivity {
     private static final String TAG = "NAVBAR";
