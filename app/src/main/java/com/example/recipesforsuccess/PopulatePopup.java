243e6757921bc79ee085baa5b841a54cf0b9c3a9
anson <all197@ucsd.edu>
2019-06-01 16:24:30 -0700

    tmp update

--- app/src/main/java/com/example/recipesforsuccess/PopulatePopup.java
+++ app/src/main/java/com/example/recipesforsuccess/PopulatePopup.java
@@ -1,65 +1,102 @@
-//package com.example.recipesforsuccess;
+package com.example.recipesforsuccess;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.google.api.Http;
+
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+class PopulatePopup extends AsyncTask<String, Void, JSONObject> {
+    private Exception exception;
+    private String ingredientName;
+
+    public PopulatePopup(String ingredient) {
+        super();
+
+        this.ingredientName = ingredient;
+    }
+
+    @Override
+    protected JSONObject doInBackground(String... urls) {
+        JSONObject result = new JSONObject();
+
+        try {
+            URL url = new URL(urls[0]);
+
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+//            con.setRequestProperty("format", "json");
+//            con.setRequestProperty("q", ingredientName);
+//            con.setRequestProperty("sort", "r");
+            con.("api_key", "LHgDB2008wpwdJEzvK2wlR7gLNv7oPzYXCVAyJVZ");
+//            con.setRequestProperty("max", "1");
+//            con.setRequestProperty("ds", "Standard Reference");
+
+            Log.d("test", "URL = " + url.toString());
+
+            /*
+            HttpURLConnection.setFollowRedirects(true);
+            con.setInstanceFollowRedirects(false);
+            con.setDoOutput(true);
+
+            OutputStreamWriter out = new OutputStreamWriter(con.getOutputStream());
+
+            // Print the response code
+            // and response message from server.
+            Log.d("test", "Response Code:"
+                    + con.getResponseCode());
+            Log.d("test", "Response Message:"
+                    + con.getResponseMessage());
+                    */
+
+
+//            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+//            urlConnection.setRequestProperty("api_key", "LHgDB2008wpwdJEzvK2wlR7gLNv7oPzYXCVAyJVZ");
+//            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
 //
-//import android.os.AsyncTask;
-//import android.util.Log;
+//            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
 //
-//import org.json.JSONObject;
-//
-//import java.io.OutputStreamWriter;
-//import java.net.HttpURLConnection;
-//import java.net.URL;
-//
-//class PopulatePopup extends AsyncTask<Void, Void, Void> {
-//    private Exception exception;
-//
-//    public PopulatePopup(String ingredient) {
-//        super();
-//
-//
-//    }
-//
-//    protected void doInBackground(String... urls) {
-//
-//    }
-//
-//
-//    // https://stackoverflow.com/questions/6343166/how-do-i-fix-android-os-networkonmainthreadexception
-//
-//    private void fillInNutrients(String ingredient) throws Exception {
-//        JSONObject ezm = new JSONObject();
-//
-//        // Query for the ndbno ID of the ingredient
-//        URL url = new URL("https://api/nal.usda.gov/ndb/nutrients/");
-//
-//        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-//        con.setRequestMethod("GET");
-//        con.setRequestProperty("format", "json");
-//        con.setRequestProperty("q", ingredient);
-//        con.setRequestProperty("sort", "r");
-//        con.setRequestProperty("api_key", "LHgDB2008wpwdJEzvK2wlR7gLNv7oPzYXCVAyJVZ");
-//        con.setRequestProperty("max", "1");
-//        con.setRequestProperty("ds", "Standard Reference");
-//
-//        HttpURLConnection.setFollowRedirects(true);
-//        con.setInstanceFollowRedirects(false);
-//        con.setDoOutput(true);
-//
-//
-//        Log.d("test", "REACHED    111111111");
-//        OutputStreamWriter out = new OutputStreamWriter(con.getOutputStream());
-//        Log.d("test", "REACHED");
-//
-//        // Print the response code
-//        // and response message from server.
-//        Log.d("test", "Response Code:"
-//                + con.getResponseCode());
-//        Log.d("test", "Response Message:"
-//                + con.getResponseMessage());
-//
-//        //            URL url = new URL("https://api/nal.usda.gov/ndb/nutrients/");
-////            HttpURLConnection con = (HttpURLConnection) url.openConnection();
-////
-////            con.setRequestMethod("GET");
-////            con.setRequestProperty("")
-//    }
-//}
+//            String line;
+//            while ((line = reader.readLine()) != null) {
+//                Log.d("test", "CONTENTS: " + line);
+//            }
+
+        } catch (Exception e) {
+            Log.d("test", "ERROR WITH QUERYING FOR NDBNO ID: " + e );
+        }
+
+
+
+
+        return null;
+    }
+
+
+    // https://stackoverflow.com/questions/6343166/how-do-i-fix-android-os-networkonmainthreadexception
+
+    private String convertToREST(LinkedHashMap<String, String> params) {
+        StringBuilder result = new StringBuilder();
+
+        for(Map.Entry<String, String> param: params.entrySet()) {
+            if (result.length() != 0) {
+                result.append("&");
+                result.append(URLEncoder.encode(param.getKey(), "UTF-8"));
+                
+            }
+        }
+
+        return result.toString();
+    }
+}
