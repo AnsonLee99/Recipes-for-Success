e562ffb1b67c10ac19148807d62fe1a2212eb150
ruc064 <ruc064@ucsd.edu>
2019-06-02 17:51:19 -0700

    bunch of recipe stufF

--- app/src/main/java/com/example/recipesforsuccess/ViewRecipeInstructions.java
+++ app/src/main/java/com/example/recipesforsuccess/ViewRecipeInstructions.java
@@ -1,13 +1,20 @@
 package com.example.recipesforsuccess;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.method.ScrollingMovementMethod;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
+
+import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
+import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
 
 public class ViewRecipeInstructions extends AppCompatActivity {
 
@@ -20,18 +27,106 @@ public class ViewRecipeInstructions extends AppCompatActivity {
         //getSupportActionBar().setDisplayShowTitleEnabled(false);
         getSupportActionBar().hide();
 
-        TextView instructions = (TextView) findViewById(R.id.instruction_text);
-        TextView recipeText = (TextView) findViewById(R.id.recipe_name);
-        ImageView imgView = (ImageView) findViewById(R.id.recipe_img);
+        //TextView instructions = (TextView) findViewById(R.id.instruction_text);
+        //TextView recipeText = (TextView) findViewById(R.id.recipe_name);
+        //ImageView imgView = (ImageView) findViewById(R.id.recipe_img);
 
+        LinearLayout layout = (LinearLayout)findViewById(R.id.recipe_instruct_page);
+        layout.setGravity(Gravity.CENTER_HORIZONTAL);
+        //findViewById(R.id.recipe_instruct_screen).setBackgroundColor(889176327);
+
+        // Star for title
+        final ImageButton favButton = (ImageButton) findViewById(R.id.fav_button);
+        favButton.setBackgroundColor(Color.WHITE);
+        favButton.setPadding(0,0,0,0);
+
+        favButton.setOnClickListener(new View.OnClickListener() {
+            Boolean saved = false;
+
+            @Override
+            public void onClick(View v) {
+                if(!saved) {
+                    favButton.setImageResource(R.drawable.yellow_star);
+                    saved = true;
+                }
+                else{
+                    favButton.setImageResource(R.drawable.plain_star);
+                    saved = false;
+
+                }
+            }
+        });
+        // Recipe Name
+        TextView recipeName = findViewById(R.id.recipe_title);
+        recipeName.setLayoutParams(new LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT));
+        recipeName.setText(getIntent().getStringExtra("recipeName"));
+        recipeName.setTextSize(28);
+        recipeName.setTextColor(Color.BLACK);
+        recipeName.setGravity(Gravity.CENTER_VERTICAL);
+        recipeName.setBackgroundColor(Color.WHITE);
+        recipeName.setPadding(50,30,0,30);
+
+        // Recipe Image
+        ImageView imgView = (ImageView)findViewById(R.id.recipe_image);
+        imgView.setLayoutParams(new LinearLayout.LayoutParams(MATCH_PARENT, 1100));
+        imgView.setPadding(0,0,0,0);
+        imgView.setScaleType(ImageView.ScaleType.FIT_XY);
+
+        // Line after recipe name
+        TextView indentSpace = findViewById(R.id.indent_space);
+        indentSpace.setLayoutParams(new LinearLayout.LayoutParams(MATCH_PARENT, 50));
+        indentSpace.setBackground(getDrawable(R.drawable.recipe_instruction_border_1));
+
+        //ingredients title
+        TextView ingredientsTitle = (TextView) findViewById(R.id.ingredientsTitle);
+        ingredientsTitle.setTextColor(Color.BLACK);
+
+        // ingredients
         Intent intent = getIntent();
-        String recipeName = intent.getStringExtra("recipeName");
+        String ingredientString = intent.getStringExtra("ingredients");
+        TextView ingredients = findViewById(R.id.ingredients_text);
+        ingredients.setBackgroundColor(Color.WHITE);
+        ingredients.setPadding(40, 75, 40, 50);
+        ingredients.setTextColor(Color.BLACK);
+        ingredients.setText(ingredientString);
 
+        //Equipment title
+        TextView equipmentTitle = (TextView) findViewById(R.id.equipmentTitle);
+        equipmentTitle.setTextColor(Color.BLACK);
+
+        //equipment
+        String equipmentString = intent.getStringExtra("equipment");
+        TextView equipment = findViewById(R.id.equipment_text);
+        equipment.setBackgroundColor(Color.WHITE);
+        equipment.setPadding(40, 75, 40, 50);
+        equipment.setTextColor(Color.BLACK);
+        equipment.setText(equipmentString);
+
+        // Line after ingredients lsit
+        TextView indentSpace3 = findViewById(R.id.indent_space3);
+        indentSpace3.setLayoutParams(new LinearLayout.LayoutParams(MATCH_PARENT, 50));
+        indentSpace3.setBackground(getDrawable(R.drawable.recipe_instruction_border_1));
+
+        // Line after ingredients lsit
+        TextView indentSpace2 = findViewById(R.id.indent_space2);
+        indentSpace2.setLayoutParams(new LinearLayout.LayoutParams(MATCH_PARENT, 50));
+        indentSpace2.setBackground(getDrawable(R.drawable.recipe_instruction_border_1));
+
+        //instructions title
+        TextView instructionsTitle = (TextView) findViewById(R.id.instructionsTitle);
+        instructionsTitle.setTextColor(Color.BLACK);
+
+        // instructions
         String instructionString = intent.getStringExtra("instructions");
+        TextView instructions = findViewById(R.id.instructions_text);
+        instructions.setBackgroundColor(Color.WHITE);
+        instructions.setPadding(40, 75, 40, 50);
+        instructions.setTextColor(Color.BLACK);
         instructions.setText(instructionString);
-        System.out.println("instructions is: " + instructionString);
-        instructions.setMovementMethod(new ScrollingMovementMethod());
-        recipeText.setText(recipeName);
+
+
+
+        // adding everything to the layout
         Picasso.with(getApplicationContext()).load(intent.getStringExtra("imgURL")).into(imgView);
 
     }
